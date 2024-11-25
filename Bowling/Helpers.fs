2ce91cf0080e0c9dbff8b9c19ceaf5b1005bd182
Vincent Rémond <vincent.remond@gmail.com>
2024-11-25 08:01:34 +0100

    first commit

---
+++ Bowling/Helpers.fs
@@ -0,0 +1,18 @@
+﻿namespace Bowling
+
+[<RequireQualifiedAccess>]
+module List =
+    let sumByI f =
+        List.mapi f >> List.fold (+) LanguagePrimitives.GenericZero
+
+    let windowedStep size step list =
+        let rec loop acc =
+            function
+            | [] -> List.rev acc
+            | list when List.length list < size -> List.rev acc
+            | list ->
+                let window = List.take size list
+                let rest = List.skip step list
+                loop (window :: acc) rest
+
+        loop [] list
