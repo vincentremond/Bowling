5f1d2df0294b5d9b2ea94a420ed0cb6bab46a1c9
Vincent Rémond <vincent.remond@gmail.com>
2025-06-14 15:57:32 +0200

    quickstart

--- Bowling.Tests/Tests.fs
+++ Bowling.Tests/Tests.fs
@@ -23,28 +23,26 @@ let parseTextSheet (str: string) : ScoreSheet =
     |> List.windowedStep 4 3
     |> List.map (fun chars ->
         match chars with
-        | [ '|' ; 'X' ; ' ' ; '|' ] -> Strike
-        | [ '|' ; CharAsInt first ; '/' ; '|' ] -> Spare first
-        | [ '|' ; CharAsInt first ; CharAsInt second ; '|' ] -> Open(first, second)
-        | [ '|' ; CharAsInt extra ; ' ' ; '|' ] -> Extra(extra)
+        | [ '|'; 'X'; ' '; '|' ] -> Strike
+        | [ '|'; CharAsInt first; '/'; '|' ] -> Spare first
+        | [ '|'; CharAsInt first; CharAsInt second; '|' ] -> Open(first, second)
+        | [ '|'; CharAsInt extra; ' '; '|' ] -> Extra(extra)
         | f -> failwith $"Invalid frame {f}"
     )
-    
 
 [<Test>]
 let ``Test multiple sheets`` () =
-    
+
     let scoreSheets = [
         "|X |X |X |X |X |X |X |X |X |X |X |X |", 300
         "|X |X |X |X |X |X |X |X |X |X |X |9 |", 299
         "|5/|5/|5/|5/|5/|5/|5/|5/|5/|5/|5 |", 150
         "|9-|9-|9-|9-|9-|9-|9-|9-|9-|9-|", 90
     ]
-    
+
     for scoreSheet, expected in scoreSheets do
-        
+
         let scores = parseTextSheet scoreSheet
         let result = ScoreCalculator.calculateTotalScore scores
-        
+
         Assert.That(result, Is.EqualTo(expected))
-    
